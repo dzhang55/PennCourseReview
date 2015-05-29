3a451bccfc0722dcd8fd3c5a8b62a8388f5f52e1
Brady Neal <bradyneal11@gmail.com>
2015-05-29 19:20:53 -0400

    Added blank SVG

--- BubbleChart/js/bubblechart.js
+++ BubbleChart/js/bubblechart.js
@@ -1,5 +1,9 @@
 var deptUrl = 'http://api.penncoursereview.com/v1/depts?token=public';
 
+var svg = d3.select("body").append("svg")
+	.attr("width", diameter)
+	.attr("height", diameter);
+
 d3.json(deptUrl, function(error, json) {
 	if (error) return console.warn(error);
 	document.write(json);
